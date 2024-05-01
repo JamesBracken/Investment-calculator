b146e6f7d8c2b925f928d6cb5e6e545ca0180213
JamesBracken <157417449+JamesBracken@users.noreply.github.com>
2024-05-01 18:06:57 +0100

    lifted state up from UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react"
+
 import { Header } from "./components/Header.jsx"
 import {UserInput} from "./components/UserInput.jsx"
 
-function App() {
+function App() {  
+const [userInput, setUserInput] = useState({
+  initialInvestment : 10000,
+  annualInvestment : 1200,
+  expectedReturn : 6,
+  duration : 10
+})  
+
   return (
-    <div>
+    <>
       <Header id='header'/>
       <UserInput />
-    </div>
+    </>
   )
 }
 
